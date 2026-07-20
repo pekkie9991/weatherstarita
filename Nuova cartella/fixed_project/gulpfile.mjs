41b410cb1cb0e491cce22eade9fe1416a0c2196c
pekkie9991 <pekkie15@gmail.com>
2026-07-21 01:31:02 +0200

    update

--- Nuova cartella/fixed_project/gulpfile.mjs
+++
@@ -1,9 +0,0 @@
-import updateVendor from './gulp/update-vendor.mjs';
-import publishFrontend from './gulp/build-gh-pages.mjs';
-
-export {
-	updateVendor,
-	publishFrontend,
-};
-
-export default publishFrontend;
--- Nuova cartella/fixed_project/server/scripts/modules/utils/experimental.mjs
+++
@@ -1,8 +0,0 @@
-export default class ExperimentalFeatures {
-	static getExperimentalFlag() {
-		const experimentalFeatures = document.documentElement.getAttribute('experimental-features');
-
-		if (experimentalFeatures === 'true') return true;
-		return false;
-	}
-}
