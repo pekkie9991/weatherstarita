c5907f7461b864eb705a058d7eb5b72e716f5b5c
pekkie9991 <pekkie15@gmail.com>
2026-07-21 02:06:12 +0200

    Fix icon mapping to use correct filenames and paths

--- scripts/italian-icons.js
+++ scripts/italian-icons.js
@@ -2,74 +2,86 @@
 // This script maps Italian weather descriptions to English icon names
 
 (function() {
-    // Store the original icon mapping function
-    const originalXV = window.IconMapping?.XV;
-    
-    // Italian to English weather condition mapping
+    // Italian to English weather condition mapping (mapped to actual icon filenames)
     const italianToEnglish = {
         // Clear/Sunny
-        "Sereno": "clear-sky",
-        "Prevalentemente sereno": "mainly-clear",
-        "Cielo sereno": "clear-sky",
+        "Sereno": "Sunny",
+        "Prevalentemente sereno": "Mostly-Clear",
+        "Cielo sereno": "Sunny",
         
         // Cloudy
-        "Parzialmente nuvoloso": "partly-cloudy",
-        "Prevalentemente nuvoloso": "overcast",
-        "Nuvoloso": "overcast",
-        "Coperto": "overcast",
-        "Cielo coperto": "overcast",
+        "Parzialmente nuvoloso": "Partly-Cloudy",
+        "Prevalentemente nuvoloso": "Mostly-Cloudy",
+        "Nuvoloso": "Cloudy",
+        "Coperto": "Cloudy",
+        "Cielo coperto": "Cloudy",
         
         // Rain
-        "Pioggia": "rain",
-        "Rovesci di pioggia deboli": "rain-showers",
-        "Rovesci": "rain-showers",
-        "Rovesci di pioggia": "rain-showers",
-        "Acquazzoni": "heavy-rain",
+        "Pioggia": "Rain",
+        "Rovesci di pioggia deboli": "Shower",
+        "Rovesci": "Shower",
+        "Rovesci di pioggia": "Shower",
+        "Acquazzoni": "Rain",
         
         // Snow
-        "Neve": "snow",
-        "Rovesci di neve": "snow-showers",
-        "Nevicata": "heavy-snow",
+        "Neve": "Snow",
+        "Rovesci di neve": "Snow-Sleet",
+        "Nevicata": "Heavy-Snow",
         
         // Thunderstorm
-        "Temporale": "thunderstorm",
-        "Temporali": "thunderstorm",
+        "Temporale": "Thunderstorm",
+        "Temporali": "Thunderstorm",
         
         // Fog
-        "Nebbia": "fog",
+        "Nebbia": "Fog",
         
         // Mixed
-        "Misto": "rain-snow",
-        "Pioggia e neve": "rain-snow"
+        "Misto": "Rain-Snow",
+        "Pioggia e neve": "Rain-Snow"
     };
     
-    // Override the icon mapping if it exists
-    if (typeof window.IconMapping !== 'undefined') {
-        window.IconMapping.XV = function(condition, timeZone, isDay) {
-            // Check if it's an Italian condition
-            if (italianToEnglish[condition]) {
-                console.log(`Mapping Italian condition "${condition}" to "${italianToEnglish[condition]}"`);
-                condition = italianToEnglish[condition];
+    // Also patch Image.src to catch dynamic loading
+    const originalImageSrc = Object.getOwnPropertyDescriptor(Image.prototype, 'src');
+    Object.defineProperty(Image.prototype, 'src', {
+        set: function(value) {
+            if (typeof value === 'string' && value.includes('.gif')) {
+                for (const [italian, english] of Object.entries(italianToEnglish)) {
+                    if (value.includes(italian + '.gif')) {
+                        // Replace Italian condition with English icon name
+                        let newValue = value.replace(italian + '.gif', english + '.gif');
+                        // Also fix path if it's looking in images/2/ but icon is in images/
+                        if (newValue.includes('images/2/') && !newValue.includes('images/2/r/')) {
+                            newValue = newValue.replace('images/2/', 'images/');
+                        }
+                        console.log(`[Italian Icons] Replacing "${italian}.gif" with "${english}.gif"`);
+                        originalImageSrc.set.call(this, newValue);
+                        return;
+                    }
+                }
             }
-            // Call original function with mapped condition
-            if (originalXV) {
-                return originalXV.call(this, condition, timeZone, isDay);
-            }
-            // Fallback to default mapping
-            return window.IconMapping.Kc(condition);
-        };
-    }
+            originalImageSrc.set.call(this, value);
+        },
+        get: function() {
+            return originalImageSrc.get.call(this);
+        }
+    });
     
-    // Alternative: patch the icon loading directly
+    // Fix moon icon paths - they should be in images/2/ not images/
     const originalLog = console.log;
     console.log = function(...args) {
-        if (args[0] && args[0].includes && args[0].includes('Unable to locate icon for:')) {
+        if (args[0] && typeof args[0] === 'string' && args[0].includes('Unable to locate icon for:')) {
             const italianCondition = args[0].replace('Unable to locate icon for: ', '');
             if (italianToEnglish[italianCondition]) {
-                originalLog.call(console, `[Italian Icons] Mapping "${italianCondition}" to "${italianToEnglish[italianCondition]}"`);
-                // Try to load the English icon instead
-                const img = new Image();
-                img.src = `images/${italianToEnglish[italianCondition]}.gif`;
+                const englishIcon = italianToEnglish[italianCondition];
+                originalLog.call(console, `[Italian Icons] Redirecting "${italianCondition}" to "${englishIcon}"`);
+                
+                // Find and replace all img elements with this Italian condition
+                document.querySelectorAll('img[src*="' + italianCondition + '.gif"]').forEach(img => {
+                    img.src = img.src.replace(italianCondition + '.gif', englishIcon + '.gif');
+                });
+                
+                // Also replace in any img elements that might be created dynamically
+                return;
             }
         }
         originalLog.apply(console, args);
